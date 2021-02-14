b658b17fba85c64032f32d32388676c697e332c3
EdwardKerckhofZoneDev <edward@zonedev.be>
2021-02-14 18:39:54 +0100

    Add: CorrectWord component

---
+++ src/components/CorrectWord.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="correct-word">
+    <span class="letter" v-for="(letter, i) in letters" :key="i">
+      {{ correctLetters.includes(letter) ? letter : '' }}
+    </span>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'CorrectWord',
+
+  props: {
+    letters: {
+      type: Array,
+      required: true,
+    },
+    correctLetters: {
+      type: Array,
+      default: () => [],
+    },
+  },
+})
+</script>
