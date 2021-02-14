fec55a653a9922dd33fb25cad4a3020ecb21da61
EdwardKerckhofZoneDev <edward@zonedev.be>
2021-02-14 18:42:13 +0100

    Add: AppAlert component

---
+++ src/components/AppAlert.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="alert-container" :class="{ show: show }">
+    <p>You have already entered this letter</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'AppAlert',
+
+  props: {
+    show: {
+      type: Boolean,
+      default: false,
+    },
+  },
+})
+</script>
