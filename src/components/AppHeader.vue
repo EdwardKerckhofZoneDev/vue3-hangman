7059e0f475e110f667b6124ddf473bcee00fad82
EdwardKerckhofZoneDev <edward@zonedev.be>
2021-02-14 18:21:19 +0100

    Add: Header component

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Vue Hangman</h1>
+  <p>Find the hidden word - Enter a letter</p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'AppHeader',
+})
+</script>
