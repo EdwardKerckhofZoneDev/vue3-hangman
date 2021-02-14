738502acc2c9d9fe8d58a9ddbb8812b0299c7cdd
EdwardKerckhofZoneDev <edward@zonedev.be>
2021-02-14 18:36:50 +0100

    Add: WrongGuessedLetters component

---
+++ src/components/WrongGuessedLetters.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="wrong-letters-container">
+    <div class="wrong-letters">
+      <p v-show="wrongLetters.length > 0">Wrong Letters</p>
+      <span v-for="(letter, i) in wrongLetters" :key="i">
+        {{ letter }} {{ i === wrongLetters.length - 1 ? '' : ', ' }}
+      </span>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'WrongGuessedLetters',
+
+  props: {
+    wrongLetters: {
+      type: Array,
+      default: () => [],
+    },
+  },
+})
+</script>
